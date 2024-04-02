df8bb77e87d282c4e94508aaada44e53bd6db789
Thomas Foster <foster.pianist@gmail.com>
2024-04-02 18:28:58 -0400

    Initial commit

---
+++ src/main/java/net/teefoss/dni/DniCraftClient.java
@@ -0,0 +1,11 @@
+package net.teefoss.dni;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DniCraftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
